aca84eb7d932c6db0ffb4f8de3e78d2bab148450
ipandeysachin <79364657+ipandeysachin@users.noreply.github.com>
2024-10-06 13:11:49 +0000

    Done

---
+++ props-components/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Greets from './Propertie-Components -(Props)/props';
+
+function App() {
+  return (
+    <div className="App">
+      <Greets />
+    </div>
+  );
+}
+
+export default App;
---
+++ props-components/src/Propertie-Components -(Props)/props.js
@@ -0,0 +1,5 @@
+function Greets() {
+  return <h1> Hello World! </h1>;
+}
+
+export default Greets;
